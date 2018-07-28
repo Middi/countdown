8737f80058fb876df270b1b89809d83c25737f49
“Richard Middleton” <Richard@richardmiddleton.me>
2018-07-28 14:53:33 +0200

    center div and add button

---
+++ client/src/components/Add/Add.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./add.css";
+
+export default class Add extends Component {
+  render() {
+    return (
+            <div className="add-btn">
+                +
+            </div>
+        )
+  }
+}
